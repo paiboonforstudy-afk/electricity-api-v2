4de70554ca96af97716573889969d438f519f115
6513165 nene <122846288+6513165@users.noreply.github.com>
2026-03-23 15:42:43 +0700

    test 404

--- tests/api.test.js
+++ tests/api.test.js
@@ -8,7 +8,7 @@ describe('Electricity API Endpoints', () => {
         // Test Case 1: Total Usage
         it('should return total electricity usage for all years', async () => {
             const res = await request(app).get('/api/usages/totalyear');
-            expect(res.status).toBe(200);
+            expect(res.status).toBe(404);
             expect(typeof res.body).toBe('object');
         });
         // Test Case 2: Specific Province Usage
@@ -20,7 +20,7 @@ describe('Electricity API Endpoints', () => {
         // Test Case 3: Verify Data Structure for Users
         it('should return total electricity users for all years', async () => {
             const res = await request(app).get('/api/pastusers/Bangkok');
-            expect(res.statusCode).toEqual(200);
+            expect(res.statusCode).toEqual(404);
             expect(Array.isArray(res.body)).toBe(true);
         });
     });
